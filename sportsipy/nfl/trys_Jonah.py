3a3b262ad9212fca46fa39a67ac45f1527ec93d7
Jonah <1jonahg1@gmail.com>
2023-03-21 17:25:51 +0200

    Update trys_Jonah.py

--- sportsipy/nfl/trys_Jonah.py
+++ sportsipy/nfl/trys_Jonah.py
@@ -9,33 +9,12 @@ import time
 import pandas as pd
 
 
-boxscore1 = Boxscore('202209150kan')
-
-print(int(boxscore1.scoring[3][-1]['current_score']['away']))
 check_game = GameData('202209150kan')
 print(check_game._gameValue)
-boxscore1 = Boxscore('202209150kan')
-print(boxscore1.winning_abbr)
-print(boxscore1.losing_abbr)
-
-
-boxscore1 = Boxscore('202209150kan')
-print(boxscore1._alt_abbreviations)
-print(boxscore1.home_abbreviation)
-
-boxscore2 = Boxscore('202209180rai')
-team1, team2 = boxscore2._alt_abbreviations(boxscore2._retrieve_html_page('202209150kan'))
-print(team1)
-print(team2)
-print(boxscore2.home_abbreviation)
-print(boxscore2.away_abbreviation)
-boxscore1 = Boxscore('202209150kan')
-print(boxscore1.winning_abbr)
-print(boxscore1.losing_abbr)
-print(boxscore1.home_abbreviation.upper())
-print(boxscore1.away_abbreviation.upper())
 
 """
-check_game = GameData('202209150kan')
-print(check_game._gameValue)
+------------------------------Gamas Checked------------------------------
+
+
+
 """
