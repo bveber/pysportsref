6eb35be91b6ef2179c594a33b229b09f0fbbbc3c
Jonah <1jonahg1@gmail.com>
2023-04-23 09:58:13 +0300

    Update testQBrating.py

--- sportsipy/nfl/testQBrating.py
+++ sportsipy/nfl/testQBrating.py
@@ -4,12 +4,12 @@ from nfl.weekly_games_Jonah import WeeklyGames
 games10 = WeeklyGames(10, 2022)
 games11 = WeeklyGames(11, 2022)
 
-sumRatings = sum(games10.qb_ratings + games11.qb_ratings) 
-amountGames = len(games10.qb_ratings) + len(games11.qb_ratings)
+sumRatings = sum(games10._qb_ratings + games11._qb_ratings) 
+amountGames = len(games10._qb_ratings) + len(games11._qb_ratings)
 
 print("amount of games: " + str(amountGames))
 print("average score: " + str(amountGames))
 print("----------games10----------")
-print(games10.qb_ratings)
+print(games10._qb_ratings)
 print("----------games11----------")
-print(games11.qb_ratings)
+print(games11._qb_ratings)
